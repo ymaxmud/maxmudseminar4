33aa06673c97128ec838c6aed6f3a23915dcfafe
Maxmud <mamudyusupov11@gmail.com>
2023-10-05 11:32:23 +0500

    init:seminar4

---
+++ README.md
@@ -0,0 +1,1 @@
+this is seminar4
---
+++ project1/main.py
@@ -0,0 +1,14 @@
+#task5 who knows what the heell we are doing
+decimal_number = input("Please type decimal ")
+decimal_to_binary = bin(int(decimal_number))
+#print("This is binary " + decimal_to_binary)
+
+
+
+
+decimal_number = input("Please type decimal ")
+decimal_to_oct = oct(int(decimal_number))
+print("This is binary " + decimal_to_binary)
+
+
+
